e3842e01a733da16a73e8e628b4295b56150a41e
Jamie Strausbaugh <jstrausb86@gmail.com>
2019-05-24 12:01:42 -0400

    Modified siteConfig projectName prop

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -30,7 +30,7 @@ const siteConfig = {
   //   baseUrl: '/test-site/',
 
   // Used for publishing and more
-  projectName: "working-docs",
+  projectName: "jamoverjelly.github.io",
   organizationName: "jamoverjelly",
   // For top-level user or org sites, the organization is still the same.
   // e.g., for the https://JoelMarcey.github.io site, it would be set like...
