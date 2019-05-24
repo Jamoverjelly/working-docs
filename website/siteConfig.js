06553c68fc0f47b85a0c642c6b84efd49b0f54d4
Jamie Strausbaugh <jstrausb86@gmail.com>
2019-05-24 12:49:55 -0400

    Updated siteConfig baseUrl prop

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -24,7 +24,7 @@ const siteConfig = {
   title: `Jamie's Docs`, // Title for your website.
   tagline: "A site for storing and viewing docs",
   url: "https://jamoverjelly.github.io", // Your website URL
-  baseUrl: "/working-docs/", // Base URL for your project */
+  baseUrl: "/working-docs/index.html", // Base URL for your project */
   // For github.io type URLs, you would set the url and baseUrl like:
   //   url: 'https://facebook.github.io',
   //   baseUrl: '/test-site/',
